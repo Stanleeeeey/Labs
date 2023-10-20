a044b579cdd0627a16531baa953f7de4c68dc3f4
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-10-20 19:42:03 +0200

    ua, ub, uc

---
+++ lib.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def ua(values:np.array):
+
+    length = len(values)
+    mean   = np.mean(values)
+
+    return np.sqrt(1/(length*(length - 1)) * np.sum(np.array([(i - mean)**2 for i in values])))
+
+def ub(accuracy: np.float64, reaction_time: np.float64 = 0.0):
+    return np.sqrt(accuracy**2 / 3 + reaction_time**3 / 3)
+
+def get_maximums(values: np.array, accuracy: np.float64,  reaction_time: np.float64 = 0.0):
+    uc =  np.sqrt(ua(values)**2 + ub(accuracy, reaction_time)**2)
+
+    mean   = np.mean(values)
+
+    return mean-uc, mean+uc
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+from lib import *
 
 data_path = "data/"
 
@@ -7,8 +9,13 @@ D_file = pd.read_csv( data_path + "capital_d.csv")
 m_file = pd.read_csv( data_path + "m.csv")
 T_file = pd.read_csv(data_path  + "T.csv")
 
+d = d_file["d[mm]"].to_numpy().astype(np.float64) / 1000
+D = D_file["D[mm]"].to_numpy().astype(np.float64) / 1000
+m = m_file["m[g]"].to_numpy().astype(np.float64) / 1000
+T = T_file["T[s]"].to_numpy().astype(np.float64) / 50
 
-print(d_file["d[mm]"])
-print(D_file["D[mm]"])
-print(m_file["m[g]"])
-print(T_file["T[s]"])
+min_d, max_d = get_maximums(d, 0.02 / 1000)
+min_D, max_D = get_maximums(D, 0.02 / 1000)
+min_m, max_m = get_maximums(m, 0.1 / 1000)
+min_T, max_T = get_maximums(T, 0.01, 0.1)
+
