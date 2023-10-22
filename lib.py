22e76be987fce2cdd7e10aca937d9335e52d61d1
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-10-22 11:40:41 +0200

    final version

--- lib.py
+++ lib.py
@@ -10,17 +10,24 @@ def ua(values:np.array):
 
     return np.sqrt(1/(length*(length - 1)) * np.sum(np.array([(i - mean)**2 for i in values])))
 
+
+
 def ub(accuracy: np.float64, reaction_time: np.float64 = 0.0):
     return np.sqrt(accuracy**2 / 3 + reaction_time**3 / 3)
 
 def get_maximums(values: np.array, accuracy: np.float64,  reaction_time: np.float64 = 0.0):
     uc =  np.sqrt(ua(values)**2 + ub(accuracy, reaction_time)**2)
-
+    
     mean   = np.mean(values)
 
     return mean-uc, mean+uc
 
-def I(T:np.float64, m: np.float64, d:np.float64):
-    return ((T**2 * m * GRAVITY * d / 2)/ (4*math.pi))
+def get_T_maximums(values: np.array, accuracy: np.float64,  reaction_time: np.float64 = 0.0):
+    uc = np.sqrt(ua(values)**2 + ub(accuracy, reaction_time)**2) 
+    print(uc)
+    mean   = np.mean(values)
+
+    return mean-uc, mean+uc
+
 
 
--- main.py
+++ main.py
@@ -14,18 +14,21 @@ T_file = pd.read_csv(data_path  + "T.csv")
 d = d_file["d[mm]"].to_numpy().astype(np.float64) / 1000
 D = D_file["D[mm]"].to_numpy().astype(np.float64) / 1000
 m = m_file["m[g]"].to_numpy().astype(np.float64) / 1000
-T = T_file["T[s]"].to_numpy().astype(np.float64) / 50
+T = T_file["T[s]"].to_numpy().astype(np.float64) /50
 
-min_d, max_d = get_maximums(d, 0.02 / 1000)
-min_D, max_D = get_maximums(D, 0.02 / 1000)
-min_m, max_m = get_maximums(m, 0.1 / 1000)
-min_T, max_T = get_maximums(T, 0.01, 0.1)
+
+
+min_d, max_d = get_maximums(d, 0.00002)
+min_D, max_D = get_maximums(D, 0.00002)
+min_m, max_m = get_maximums(m, 0.0001)
+min_T, max_T = get_T_maximums(T, 0.01, 0.1)
 
 
 d = np.mean(d)
 D = np.mean(D)
-m = np.mean(m)
+m = np.mean(m)  
 T = np.mean(T)
+
 
 
 #wyliczanie I
@@ -42,9 +45,10 @@ print(f"I: {I} +- {uI}")
 #wyliczanie I_0 na podstawie tw steinera
 min_I, max_I = I- uI, I+uI
 
-u_mI0 = 0.5 * np.abs((I - min_m * (d/2)**2) - (I - max_m * (d/2)**2))
-u_dI0 = 0.5 * np.abs((I - m * (min_d/2)**2) - (I - m * (max_d/2)**2))
-u_II0 = 0.5 * np.abs((min_I - m * (d/2)**2) - (max_I - m * (d/2)**2))
+u_mI0 = 0.5 * np.abs((I - max_m * (d/2)**2) - (I - min_m * (d/2)**2))
+u_dI0 = 0.5 * np.abs((I - m * (max_d/2)**2) - (I - m * (min_d/2)**2))
+u_II0 = 0.5 * np.abs((max_I - m * (d/2)**2) - (min_I - m * (d/2)**2))
+
 
 I0   = (I - m * (d/2)**2)
 u_I0 = np.sqrt(u_mI0**2 + u_dI0**2 + u_II0**2)
@@ -52,11 +56,12 @@ u_I0 = np.sqrt(u_mI0**2 + u_dI0**2 + u_II0**2)
 print(f"I0 ze Steinera: {I0} +- {u_I0}")
 
 #wyliczanie I_0 z teori 
-u_mI0 = 0.5 * np.abs((1/8 * min_m * (d**2 + D**2)) - (1/8 * max_m * (d**2 + D**2)))
-u_dI0 = 0.5 * np.abs((1/8 * m * (min_d**2 + D**2)) - (1/8 * m * (max_d**2 + D**2)))
-u_DI0 = 0.5 * np.abs((1/8 * m * (d**2 + min_D**2)) - (1/8 * m * (d**2 + max_D**2)))
+u_mI0 = 0.5 * np.abs((max_m * (d**2 + D**2) / 8) - (min_m * (d**2 + D**2) / 8))
+u_dI0 = 0.5 * np.abs((m * (max_d**2 + D**2) / 8) - (m * (min_d**2 + D**2) / 8))
+u_DI0 = 0.5 * np.abs((m * (d**2 + max_D**2) / 8) - (m * (d**2 + min_D**2) / 8))
 
 I0   = 1/8 * m * (d**2 + D**2)
 u_I0 = np.sqrt(u_mI0**2 + u_dI0**2 + u_DI0**2)
 
 print(f"I0 z Teorii: {I0} +- {u_I0}")
+
