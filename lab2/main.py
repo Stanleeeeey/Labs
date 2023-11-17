f7bb320da6e6e0cf2b44a998e814107a7890e688
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-11-17 17:32:54 +0100

    lab2 v1.0

--- lab2/main.py
+++ lab2/main.py
@@ -19,12 +19,28 @@ d = d_file["d[mm]"].to_numpy().astype(np.float64) / 1000
 h = h_file["h[mm]"].to_numpy().astype(np.float64) / 1000
 t = t_file["t[s]"].to_numpy().astype(np.float64)
 
+#ustawienie gęstości
+density = 950 * 10**-3
+
 #wyliczanie maksimum i minimum
 d_min, d_max = get_maximums(d, 0.01 / 1000)
 h_min, h_max = get_maximums(h, 1 / 1000)
 t_min, t_max = get_maximums(t, 0.01, 0.5)
 
-d_mean = np.mean(d)
-h_mean = np.mean(h)
-t_mean = np.mean(t)
 
+#wyliczenie średnich
+d = np.mean(d)
+h = np.mean(h)
+t = np.mean(t)
+
+
+u_Ft_d = 0.5 *np.abs(6 * np.pi * density * d_max * h / t - 6 * np.pi * density * d_min * h / t)
+u_Ft_h = 0.5 *np.abs(6 * np.pi * density * d * h_max / t - 6 * np.pi * density * d * h_min / t)
+u_Ft_t = 0.5 *np.abs(6 * np.pi * density * d * h / t_max - 6 * np.pi * density * d * h / t_min)
+
+
+u_Ft = np.sqrt(u_Ft_d ** 2 + u_Ft_h ** 2 + u_Ft_t ** 2)
+
+Ft = 6 * np.pi * density * d * h / t
+
+print(f"F_t = {Ft} N +- {u_Ft}")
