0393647bf3346a0c24320d13450ba430047fc38c
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-16 17:38:08 +0100

    fix

--- lab3/main.py
+++ lab3/main.py
@@ -26,7 +26,7 @@ U_mean = np.mean(U)
 
 T_precision = T*0.05*0.01 + 0.5 #K
 U_precision = U*0.01 + 2*0.1 #V
-I_precision = U*2*0.01 + 2*0.01 #A
+I_precision = I*2*0.01 + 2*0.01 #A
 L_precision = 0.01 #mm
 
 u_T = [ub(t) for t in T_precision]
@@ -36,6 +36,8 @@ u_L = [ub(L_precision) for i in delta_l]
 
 L_0 = 0.905
 u_L_0 = 0.004
+
+
 
 L_min, L_max = delta_l - u_L,delta_l+u_L 
 U_min, U_max = U - u_U      ,U+u_U
@@ -47,9 +49,10 @@ L_0_min, L_0_max = L_0 - u_L_0, L_0 + u_L_0
 u_alpha_L = 0.5 *np.abs(L_min / (L_0 * (T-T_0)) - L_max / (L_0 * (T-T_0)))
 u_alpha_L_0 = 0.5 *np.abs(delta_l / (L_0_max * (T-T_0)) - delta_l / (L_0_min * (T-T_0)))
 u_alpha_T = 0.5 *np.abs(delta_l / (L_0 * (T_max-T_0)) - delta_l / (L_0 * (T_min-T_0)))
+u_alpha_T_0 = 0.5 *np.abs(delta_l / (L_0 * (T-T_0 + u_T[0])) - delta_l / (L_0 * (T-T_0-u_T[0])))
 
 
-u_alpha = np.sqrt(u_alpha_L**2 + u_alpha_L_0**2 + u_alpha_T**2)
+u_alpha = np.sqrt(u_alpha_L**2 + u_alpha_L_0**2 + u_alpha_T**2+ u_alpha_T_0**2 )
 
 alpha = delta_l / (L_0 * (T-T_0))
 print(f"alpha = {alpha} +- {u_alpha}")
@@ -58,11 +61,13 @@ u_L_L_by_L_0 = 0.5 *np.abs(L_max/L_0 - L_min/L_0)
 u_L_0_L_by_L_0 = 0.5 *np.abs(delta_l/L_0_max - delta_l/L_0_min)
 
 u_L_by_L_0 = np.sqrt(u_L_L_by_L_0**2 + u_L_0_L_by_L_0**2)
-print(u_T)
+
+
+
 plt.errorbar(y= delta_l/L_0, x=T-T_0, yerr=u_L_by_L_0, xerr=u_T ,fmt='o', capsize=5, markersize = 0, label='Dane z błędami')
-plt.axis('equal')
+
 plt.xlim(0)  
-plt.ylim(0) 
+plt.ylim(-0.01, 0.01) 
 
 coefficients = np.polyfit(delta_l/L_0, T-T_0, 1)
 polynomial = np.poly1d(coefficients)
