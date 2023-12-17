d9f4e7eb08e9dc9f1f41115c8626bb6abcab3928
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-17 12:14:06 +0100

    draft one

--- lab3/main.py
+++ lab3/main.py
@@ -11,12 +11,15 @@ I_file       = pd.read_csv( data_path + "I.csv")
 T_file       = pd.read_csv( data_path + "T.csv")
 U_file       = pd.read_csv( data_path + "U.csv")
 
-T_0 = 23.3
+T_0 = 22.8
+T_0_precision =  T_0*0.05*0.01 + 0.5 #K
+u_T_0 = ub(T_0_precision)
 
-delta_l = delta_l_file["delta_L[mm]"].to_numpy().astype(np.float64)[1:] / 1000
-I       = I_file["I[A]"].to_numpy().astype(np.float64)[1:]
-T       = T_file["T[C]"].to_numpy().astype(np.float64)[1:]
-U       = U_file["U[V]"].to_numpy().astype(np.float64)[1:]
+
+delta_l = delta_l_file["delta_L[mm]"].to_numpy().astype(np.float64) / 1000
+I       = I_file["I[A]"].to_numpy().astype(np.float64)
+T       = T_file["T[C]"].to_numpy().astype(np.float64)
+U       = U_file["U[V]"].to_numpy().astype(np.float64)
 
 
 L_mean = np.mean(delta_l)
@@ -27,7 +30,9 @@ U_mean = np.mean(U)
 T_precision = T*0.05*0.01 + 0.5 #K
 U_precision = U*0.01 + 2*0.1 #V
 I_precision = I*2*0.01 + 2*0.01 #A
-L_precision = 0.01 #mm
+L_precision = 0.01 / 1000 #m
+
+
 
 u_T = [ub(t) for t in T_precision]
 u_U = [ub(u) for u in U_precision]
@@ -49,8 +54,7 @@ L_0_min, L_0_max = L_0 - u_L_0, L_0 + u_L_0
 u_alpha_L = 0.5 *np.abs(L_min / (L_0 * (T-T_0)) - L_max / (L_0 * (T-T_0)))
 u_alpha_L_0 = 0.5 *np.abs(delta_l / (L_0_max * (T-T_0)) - delta_l / (L_0_min * (T-T_0)))
 u_alpha_T = 0.5 *np.abs(delta_l / (L_0 * (T_max-T_0)) - delta_l / (L_0 * (T_min-T_0)))
-u_alpha_T_0 = 0.5 *np.abs(delta_l / (L_0 * (T-T_0 + u_T[0])) - delta_l / (L_0 * (T-T_0-u_T[0])))
-
+u_alpha_T_0 = 0.5 *np.abs(delta_l / (L_0 * (T-T_0 + u_T_0)) - delta_l / (L_0 * (T-T_0-u_T_0)))
 
 u_alpha = np.sqrt(u_alpha_L**2 + u_alpha_L_0**2 + u_alpha_T**2+ u_alpha_T_0**2 )
 
@@ -64,15 +68,15 @@ u_L_by_L_0 = np.sqrt(u_L_L_by_L_0**2 + u_L_0_L_by_L_0**2)
 
 
 
-plt.errorbar(y= delta_l/L_0, x=T-T_0, yerr=u_L_by_L_0, xerr=u_T ,fmt='o', capsize=5, markersize = 0, label='Dane z błędami')
+plt.errorbar(y= delta_l/L_0, x=T, yerr=u_L_by_L_0, xerr=u_T ,fmt='o', capsize=5, markersize = 0, label='Dane z błędami')
 
 plt.xlim(0)  
-plt.ylim(-0.01, 0.01) 
+plt.ylim(0) 
 
-coefficients = np.polyfit(delta_l/L_0, T-T_0, 1)
-polynomial = np.poly1d(coefficients)
-
+a, b = np.polyfit(T, delta_l/L_0, 1)
 x_reg = np.linspace(0, 150, 1000) 
+print(f"y = {a}*x + {b}")
 
-plt.plot(coefficients[0]*x_reg + coefficients[1], x_reg)
+
+plt.plot(x_reg, a*x_reg + b, x_reg)
 plt.show()
