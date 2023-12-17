0033d98d3a7b9b0926cab75c14c77709767c490a
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2023-12-17 14:53:06 +0100

    final

--- lab3/main.py
+++ lab3/main.py
@@ -15,7 +15,7 @@ T_0 = 22.8
 T_0_precision =  T_0*0.05*0.01 + 0.5 #K
 u_T_0 = ub(T_0_precision)
 
-
+print(u_T_0 )
 delta_l = delta_l_file["delta_L[mm]"].to_numpy().astype(np.float64) / 1000
 I       = I_file["I[A]"].to_numpy().astype(np.float64)
 T       = T_file["T[C]"].to_numpy().astype(np.float64)
@@ -33,16 +33,15 @@ I_precision = I*2*0.01 + 2*0.01 #A
 L_precision = 0.01 / 1000 #m
 
 
-
 u_T = [ub(t) for t in T_precision]
 u_U = [ub(u) for u in U_precision]
 u_I = [ub(i) for i in I_precision]
 u_L = [ub(L_precision) for i in delta_l]
 
+print(u_I)
+
 L_0 = 0.905
 u_L_0 = 0.004
-
-
 
 L_min, L_max = delta_l - u_L,delta_l+u_L 
 U_min, U_max = U - u_U      ,U+u_U
@@ -59,24 +58,60 @@ u_alpha_T_0 = 0.5 *np.abs(delta_l / (L_0 * (T-T_0 + u_T_0)) - delta_l / (L_0 * (
 u_alpha = np.sqrt(u_alpha_L**2 + u_alpha_L_0**2 + u_alpha_T**2+ u_alpha_T_0**2 )
 
 alpha = delta_l / (L_0 * (T-T_0))
-print(f"alpha = {alpha} +- {u_alpha}")
+print(f"mean alpha = {np.mean(alpha)} +- {np.mean(u_alpha)}")
 
 u_L_L_by_L_0 = 0.5 *np.abs(L_max/L_0 - L_min/L_0)
 u_L_0_L_by_L_0 = 0.5 *np.abs(delta_l/L_0_max - delta_l/L_0_min)
 
 u_L_by_L_0 = np.sqrt(u_L_L_by_L_0**2 + u_L_0_L_by_L_0**2)
 
+u_P_I = 0.5*np.abs(U_min*I - U_max*I)
+u_P_U = 0.5*np.abs(U*I_min - U*I_max)
+
+u_P = np.sqrt(u_P_I**2 + u_P_U**2)
 
 
-plt.errorbar(y= delta_l/L_0, x=T, yerr=u_L_by_L_0, xerr=u_T ,fmt='o', capsize=5, markersize = 0, label='Dane z błędami')
+print(u_P_I[0], u_P_U[0], u_P[0])
+P = U*I
 
-plt.xlim(0)  
-plt.ylim(0) 
 
-a, b = np.polyfit(T, delta_l/L_0, 1)
+u_delta_T_T = 0.5*np.abs((T-T_0 + u_T) - (T-T_0 - u_T))
+u_delta_T_T_0 = 0.5*np.abs((T-T_0 + u_T_0) - (T-T_0 - u_T_0))
+
+u_delta_T = np.sqrt(u_delta_T_T **2 + u_delta_T_T_0**2)
+
+plt.ylabel("ΔL / L₀")
+plt.xlabel("ΔT[°C]")
+plt.xlim(0, 130)  
+plt.ylim(0, 0.0021) 
+plt.grid()
+
+a, b = np.polyfit(T- T_0, delta_l/L_0, 1)
 x_reg = np.linspace(0, 150, 1000) 
 print(f"y = {a}*x + {b}")
 
 
-plt.plot(x_reg, a*x_reg + b, x_reg)
-plt.show()
+plt.plot(x_reg, a*x_reg + b, x_reg, color = "lightsteelblue")
+plt.errorbar(y= delta_l/L_0, x=T-T_0, yerr=u_L_by_L_0, xerr=u_delta_T ,fmt='o', capsize=5, markersize = 0, color = "blue")
+plt.savefig("x.png", bbox_inches='tight')
+
+
+plt.clf()
+
+plt.ylabel("P[W]")
+plt.xlabel("ΔT[°C]")
+plt.xlim(0, 130)
+plt.ylim(0, 15)
+plt.grid()
+
+a1, b1 = np.polyfit(T - T_0, P, 1)
+x_reg = np.linspace(0, 150, 1000)
+print(f"y = {a1}*x + {b1}")
+plt.plot(x_reg, a1 * x_reg + b1, color="lightsteelblue")  # Remove the second argument x_reg
+plt.errorbar(y=P, x=T - T_0, yerr=u_P, xerr=u_delta_T, fmt='o', capsize=5, markersize=0, color="blue")
+
+plt.savefig("y.png", bbox_inches='tight')
+
+
+for i in range(len(u_T)):
+    print(f"{i+1} & ${u_L_L_by_L_0[i]}$ & ${u_L_0_L_by_L_0[i]}$ & ${u_L_by_L_0[i]}$ & ${u_delta_T_T[i]}$ & ${u_delta_T_T_0[i]}$ & ${u_delta_T[i]}$")
